b4cf0b399fa32d05b9b84a447e75c0df74710d2b
Vladimir Perfilev <persoff68@gmail.com>
2022-12-27 22:07:32 +0100

    TDM-176 update feedback api url

--- src/constants.ts
+++ src/constants.ts
@@ -2,7 +2,7 @@ export const RECAPTCHA_KEY = '6Le5TKQZAAAAANv8EpvGFrvrgoP7HFVMPnPzu-55';
 
 export const LOGIN_URL = 'https://client.fatodo.app/login';
 export const REGISTRATION_URL = 'https://client.fatodo.app/registration';
-export const FEEDBACK_API = 'https://todo';
+export const FEEDBACK_API = 'https://api.fatodo.com/api/auth/feedback';
 
 export const GOOGLE_PLAY_URL = 'https://todo';
 export const APPLE_STORE_URL = 'https://todo';
