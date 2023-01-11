9ed5c4f8d15b003b8c498afeb53650648fc0a32f
Vladimir Perfilev <persoff68@gmail.com>
2023-01-11 11:47:27 +0100

    TDM-184 add link to app store

--- src/constants.ts
+++ src/constants.ts
@@ -5,4 +5,4 @@ export const REGISTRATION_URL = 'https://client.fatodo.app/registration';
 export const FEEDBACK_API = 'https://api.fatodo.app/api/auth/feedback';
 
 export const GOOGLE_PLAY_URL = 'https://play.google.com/store/apps/details?id=app.fatodo';
-export const APPLE_STORE_URL = 'https://apps.apple.com/';
+export const APPLE_STORE_URL = 'https://apps.apple.com/app/fatodo/id1663058112';
