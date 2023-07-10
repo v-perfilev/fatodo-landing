bd9b349d04949040a444f67ce7489b2be146c0dd
Vladimir Perfilev <persoff68@gmail.com>
2023-07-11 00:35:07 +0200

    edit logs to authorization-check.js

--- static/scripts/authorization-check.js
+++ static/scripts/authorization-check.js
@@ -4,37 +4,17 @@ const SCRIPT_IFRAME_ID = 'auth_ifr';
 
 function addIframeToBody() {
   const iframe = document.createElement('iframe');
+  iframe.addEventListener('load', function () {
+    console.log('loaded!');
+    iframe.contentWindow.postMessage('*', '*');
+  });
   iframe.setAttribute('id', SCRIPT_IFRAME_ID);
   iframe.setAttribute('style', 'display:none;');
   iframe.setAttribute('src', SCRIPT_AUTH_URL);
   document.body.appendChild(iframe);
 }
 
-function sendRequest() {
-  const iframe = document.getElementById(SCRIPT_IFRAME_ID);
-  let win;
-  try {
-    win = iframe.contentWindow;
-  } catch (e) {
-    win = iframe.contentWindow;
-  }
-
-  const inter = window.setInterval(function () {
-    if (win.document.readyState === 'complete') {
-      window.clearInterval(inter);
-      console.log('post!!!!!');
-      win.postMessage('*', '*');
-    }
-  }, 50);
-}
-
-function checkAuthorization() {
-  console.log('Fatodo Authorization Check: started');
-  addIframeToBody();
-  sendRequest();
-}
-
-window.addEventListener('message', function (event) {
+function onMessage(event) {
   console.log(event);
   if (event.origin === SCRIPT_APP_DOMAIN) {
     console.log('Fatodo Authorization Check: response received');
@@ -42,10 +22,12 @@ window.addEventListener('message', function (event) {
       window.location.replace(SCRIPT_APP_DOMAIN);
     }
   }
-});
+}
+
+window.addEventListener('message', onMessage, false);
 
 if (document.body != null) {
-  checkAuthorization();
+  addIframeToBody();
 } else {
-  window.onload = checkAuthorization;
+  window.onload = addIframeToBody;
 }
